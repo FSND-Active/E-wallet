2ac44519f4b59c0e96671f0af97c792c976bb8c7
Godand <aezeigwe2003@gmail.com>
2022-10-24 11:27:22 -0400

    project template

---
+++ Backend/flaskr/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask, request,jsonify,abort
+from flask_cors import CORS
+from models import setup_db, model_name
+
+def create_app(test_config=None):
+    app=Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Origin", "*"
+        )
+        return response
+
+    @app.route("/",methods=["GET"])
+    def hello():
+        return "hello world"
---
+++ Backend/models.py
@@ -0,0 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+database_name = os.getenv('DB_NAME','wallet_test')
+database_user= os.getenv('DB_USER','test_user')
+database_password= os.getenv('DB_PASS','test_password')
+database_network=os.getenv('DB_NET','localhost:5432')
+database_path = 'postgresql://{}:{}@{}/{}'.format(database_user,database_password,database_network, database_name)
+
+db= SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+"""
+Sample model
+
+"""
+
+class model_name(db.Model):
+    __tablename__=""
+
+    id = db.Column(db.Integer, primary_key=True)
+    args= db.Column(db.String)
+    #other topics
+
+    def __init__(self, id, args):
+        self.args=args
+        self.id=id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format (self):
+        return {"id":self.id}
